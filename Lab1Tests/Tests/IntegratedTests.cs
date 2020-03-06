93c5c86858edbd4d7e3ba197df20ecf7e6e9e9bd
Nikita Yazykov <cour1337@gmail.com>
2020-03-05 18:09:55 -0800

    added integration tests

---
+++ Lab1Tests/Tests/IntegratedTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ArrayProcessForm.Modules;
+using System.Collections.Generic;
+
+namespace Lab1Tests.Tests
+{
+    [TestClass]
+    public class IntegratedTests
+    {
+        [TestMethod]
+        public void DigitAverageAndDigitCountTest()
+        {
+            string s = "8820";
+            int digitCount = DigitCountCalculator.Calculate(s);
+            float digitSum = 18;
+            Assert.AreEqual(digitSum / digitCount, DigitAverageCalculator.Calculate(s));
+        }
+
+        [TestMethod]
+        public void AllModulesIntegration()
+        {
+            List<int> testArray = new List<int> { 0, 1234, 7100 };
+            int value = 0;
+            float average = 2;
+            int digitSum = 8;
+            foreach (int testNumber in testArray)
+            {
+                value = DigitAverageCalculator.Calculate(testNumber) < 7 && MaxDigitFinder.Find(testNumber) == 7 ? testNumber : value;
+            }
+            Assert.AreEqual(2, digitSum / DigitCountCalculator.Calculate(value));
+            Assert.AreEqual(7100, value);
+
+        }
+    }
+}
